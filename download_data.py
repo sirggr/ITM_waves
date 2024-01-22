0b163f67f310919795538cdbd1ecb28ae5d9b726
sirggr <137459195+sirggr@users.noreply.github.com>
2024-01-22 12:59:33 -0500

    Define download_range function to take datetime params
    Downloads de2 measurements from from the following instruments over the specified range:
    
    - nacs
    - rpa
    - lang

--- download_data.py
+++ download_data.py
@@ -2,18 +2,16 @@ import pysat
 
 import pysatNASA
 
-import datetime as dt
-
 from pysatNASA.instruments.methods import de2
 
-# Define date range to download data
-start = dt.datetime(1983, 1, 1)
-stop = dt.datetime(1983, 3, 15)
+# Define params in dt.datetime format
+def download_range(start, stop):
+    
 
-# Download data
-nacs = pysat.Instrument('de2', 'nacs')
-nacs.download(start, stop)
-rpa = pysat.Instrument('de2', 'rpa')
-rpa.download(start, stop)
-lang = pysat.Instrument('de2', 'lang')
-lang.download(start, stop)
+    # Download data
+    nacs = pysat.Instrument('de2', 'nacs')
+    nacs.download(start, stop)
+    rpa = pysat.Instrument('de2', 'rpa')
+    rpa.download(start, stop)
+    lang = pysat.Instrument('de2', 'lang')
+    lang.download(start, stop)
